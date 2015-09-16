813e5fcbffb426304780d8e6dad6e38ff4b74bb0
Igor P. Leroy <ip.leroy@gmail.com>
2015-09-15 22:39:48 -0300

    Release 2.0.0

--- pagarme/__init__.py
+++ pagarme/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '2.0.0-dev'
-__description__ = 'Pagar.me Python library'
-__long_description__ = ''
+__version__ = '2.0.0'
+__description__ = 'Pagar.me Python'
+__long_description__ = 'Python library for the pagar.me gateway'
 
 __author__ = 'Allisson Azevedo, Marcus Bodock, Igor P. Leroy'
 __author_email__ = 'allisson@gmail.com, mbodock@gmail.com, ip.leroy@gmail.com'
