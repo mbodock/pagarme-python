192ba8b2bfa138662cd25f3502fd376187ca3e73
Igor P. Leroy <ip.leroy@gmail.com>
2015-09-15 22:19:45 -0300

    Set global stats for pagarme-python

--- pagarme/__init__.py
+++ pagarme/__init__.py
@@ -1,4 +1,8 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
+
+__version__ = '2.0.0-dev'
+__description__ = 'Pagar.me Python library'
+__long_description__ = ''
 
 from .pagarme import Pagarme
 from .exceptions import *
