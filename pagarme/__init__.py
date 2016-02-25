21c658387a60a9fdfd75f43f47cac80311d03a7c
Victor Gama <hey@vito.io>
2016-02-25 15:48:48 -0300

    Add __all__ magic to __init__.py

--- pagarme/__init__.py
+++ pagarme/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from .card import Card
 from .customer import Customer
 from .pagarme_facade import PagarmeFacade as Pagarme
@@ -6,3 +7,5 @@ from .subscription import Plan, Subscription
 from .transaction import Transaction
 
 from .exceptions import *
+
+__all__ = ['Card', 'Customer', 'Pagarme', 'Plan', 'Subscription', 'Transaction']
